7e7b08f895ba6ff2cf95a94ccd55c534cb3b94a1
litoninot12345@gmail.com <litoninot12345@gmail.com>
2022-03-16 12:15:08 +0700

    add sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use App\Traits\TraitModel;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable,HasApiTokens;
 
     use TraitModel;
 
